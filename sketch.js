a73a1e6bccefb8675adac0cfc14e991f8c8e92e2
Murphy Mulholland <Murphytekau@gmail.com>
2016-10-26 19:40:49 +1300

    fix

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 //Set up website layout and add files, link to shit
 //Run initial population creation, randomized draw.
 function setup() {
@@ -10,4 +9,3 @@ function draw() {
   fill(100);
   rect(0, 0, 600, 600);
 }
->>>>>>> 58ae662de9ce4fde43b5a8968bb9f27e1d393492
