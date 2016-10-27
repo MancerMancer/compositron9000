5fa0c80fb865ea7425f473f9b5f92836de5d44bd
Patrick Tuohy <Patrick Tuohy>
2016-10-27 17:02:01 +1300

    Sketch is a bit of a mess, but added mutation and selection to population.js

--- population.js
+++ population.js
@@ -11,15 +11,33 @@ function Population(){
     this.songs[i] = new Song();
   }
 
-this.selection = function() {
-  for (var i = 0; i < this.songs.length; i ++) {
-    var a = floor(random(this.matingPool.length));
-    var b = floor(random(this.matingPool.length));
-    var partnerA = this.matingPool[a];
-    var partnerB = this.matingPool[b];
-    var child = partnerA.crossover(parentB);
-    child.mutate(this.mutationRate);
-    this.songs[i] = child;
+  this.selection = function() {
+    for (var i = 0; i < this.songs.length; i ++) {
+      var a = floor(random(this.matingPool.length));
+      var b = floor(random(this.matingPool.length));
+      var partnerA = this.matingPool[a];
+      var partnerB = this.matingPool[b];
+      var child = partnerA.crossover(parentB);
+      child.mutate(this.mutationRate);
+      this.songs[i] = child;
+    }
+    this.generations ++;
   }
-  this.generations ++;
+
+  this.crossover = function(partner) {
+    var child = new Song(this.genes.length); //genes refers to contents of song (instr and midi)
+    for (car i = 0; < this.genes.length; i++) {
+      if (random(0,1) > 0.5) child.genes[i] = this.genes[i];
+      else child.genes[i] = partner.genes[i];
+    }
+    return child;
+  }
+
+  this.mutate = function(mutationRate) {
+    for (var i = 0; i < this.genes.length; i++) {
+      if (random(1) < mutationRate) {
+        this.genes[i] = floor(random(0, 10));
+      }
+    }
+  }
 }
