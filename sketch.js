a25cd5f8b8d744a6322b518fb4c3c6b551702771
Murphy Mulholland <Murphytekau@gmail.com>
2016-10-27 17:50:01 +1300

    fixed sketch conflicts

--- sketch.js
+++ sketch.js
@@ -4,12 +4,10 @@
 var pop;
 
 function setup() {
-<<<<<<< HEAD
 
-=======
   createCanvas(600, 600);
   pop = new Population();
->>>>>>> 23acda08fa4255ea6f0d54646b5ac8db31f2bc08
+
 }
 
 function draw() {
